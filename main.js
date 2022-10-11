20b80759f6080785aa12bd34ea10b26ef738ec26
akash12patel12 <akash12patel12@gmail.com>
2022-10-11 13:48:09 +0530

    4

--- main.js
+++ main.js
@@ -3,4 +3,14 @@
 
 var items = document.querySelectorAll('.list-group-item');
 items[2].style.background = 'green';
-items.forEach((item)=>item.style.color='blue')
+items.forEach((item)=>{item.style.color='purple';
+item.style.fontWeight ='bold';})
+
+var newliv = document.getElementsByClassName('new-li');
+
+newliv[0].textContent = "Changed it to this text by javascript using getelementsbyclassname";
+
+var nt = document.getElementsByTagName('li');
+let l = nt.length;
+
+nt[l-1].textContent ="edited it with tag name"
