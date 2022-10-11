5078595b74b6e6943e5b3cfe207ac151b7b2ef85
akash12patel12 <akash12patel12@gmail.com>
2022-10-11 12:55:55 +0530

    final

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 console.log(document);
 document.title = '/ '
 console.log(document.domain);
+console.log(document.URL);
+console.log(document.doctype);
+console.log(document.all);
+console.log(document.all[10]);
+console.log(document.forms)
+var headerTitle = document.getElementById('header-title');
+var header = document.getElementById('main-header');
+header.style.borderBottom = 'solid 4px #ccc';
+var t = document.querySelector('.title').firstChild;
+t.textContent.style.fontWeight = 'bold'
+t.textContent.style.color ='green'
